1e47ca7b5c2d860f93befab5bff1263034dfaa50
SheriffAS <lukas.pastorek@akademiasovy.sk>
2018-11-18 19:45:41 +0100

    Listeners

---
+++ CLIENT/js/Listeners.js
@@ -0,0 +1,26 @@
+function onClick(id)
+{
+  switch(id)
+  {
+    case 1://()Assembler;
+        break;
+    case 2://()Pascal;
+        break;
+    case 3://()Visual Basic;
+        break;
+    case 4://()C;
+        break;
+    case 5://()C#;
+        break;
+    case 6://()Javascript;
+        break;
+    case 7://()Java;
+        break;
+    case 8://()Python;
+        break;
+  }
+}
+
+function onProgrammerClick()
+{
+}
